465014d84f3ed9ad061d9c1bb345346740153ef7
Thomas Newton <tomnewtonx@gmail.com>
2021-06-26 22:17:32 +0100

    Various models and all unis added to db

--- UCAS_clear/models.py
+++ UCAS_clear/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class University(models.Model):
+    university_name = models.CharField(max_length=255, default=None)
+
+    def __str__(self):
+        return self.title
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=255, default=None)
+    ucas_points = models.IntegerField(default=None)
+    course_spaces = models.IntegerField(default=None)
+    availability_status = models.CharField(max_length=255, default='Available')
+    closing_date = models.DateField(default=None)
+
+    def __str__(self):
+        return self.course_name
